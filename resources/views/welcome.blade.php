cbc67789da12723e8ff9aff0f532de477e0972a0
Lucasdip <lucaslimadip@gmail.com>
2026-01-27 21:45:57 -0300

    Feito em casa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,12 @@
         <h1>
             Habitos!
         </h1>
+
+        @auth
+            <p>
+                @dd(auth()->user()->email)
+                Bem Vindo, {{ auth()->user()->name }}
+            </p>
+        @endauth
     </main>
 </x-layout>
